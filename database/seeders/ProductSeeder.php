e60805d0a3c890bd0d06b01d1b9cf62bd04e3c20
samk257 <kubuntusampras.sk@gmail.com>
2022-11-22 14:13:53 +0200

    initialisation of laravel,Product Model,actory,Seeder.Auth with Sanctum

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->create(
+            'name'
+        );
+    }
+}
